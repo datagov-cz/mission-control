d1c72c0f79df034785e3bd9929a55dcc4853d6fe
Karel Klíma <karelklima@gmail.com>
2020-04-30 17:24:58 +0200

    Switched to new SGoV server

--- src/id/constants.tsx
+++ src/id/constants.tsx
@@ -6,14 +6,14 @@ export const LOGIN_URL = '/j_spring_security_check'
 /**
  * Endpoint to fetch current authenticated user identity
  */
-export const MY_ID_URL = '/rest/users/current'
+export const MY_ID_URL = '/users/current'
 
 /**
  * Endpoint to check whether the username already exists, returns boolean
  */
-export const USERNAME_EXISTS_URL = '/rest/users/username?username='
+export const USERNAME_EXISTS_URL = '/users/username?username='
 
 /**
  * Endpoint for registering the user
  */
-export const REGISTRATION_URL = '/rest/users'
+export const REGISTRATION_URL = '/users'
---
+++ src/users/constants.ts
@@ -0,0 +1,4 @@
+/**
+ * Endpoint to fetch users
+ */
+export const USERS_URL = '/users'
