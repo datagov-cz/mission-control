538fff8ffce5f23bfac422d70ededc2b7b77e506
Karel Klíma <karelklima@gmail.com>
2020-05-06 17:36:18 +0200

    Added user profile page

---
+++ src/id/components/MeLayout.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Box, Container } from '@material-ui/core'
+import { useSelector } from 'react-redux'
+
+import { Namespace } from 'app/components/i18n'
+import RouteComponentRenderer from 'app/components/RouteComponentRenderer'
+import Gravatar from './Gravatar'
+import { getInitials, getUsername } from 'id/selectors'
+
+const MeLayout: React.FC = () => {
+  const initials = useSelector(getInitials)
+  const email = useSelector(getUsername)
+  return (
+    <Namespace.Provider value="id">
+      <Container className="Me">
+        <Box width={200} mx="auto" pt={5} pb={2}>
+          <Gravatar email={email} initials={initials} size="huge" />
+        </Box>
+        <RouteComponentRenderer />
+      </Container>
+    </Namespace.Provider>
+  )
+}
+
+export default MeLayout
