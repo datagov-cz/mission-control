8bf2ae3efb663b3335b30bf25da1db10eb2dd659
Karel Klíma <karelklima@gmail.com>
2020-05-15 11:01:59 +0200

    Added ofSafeType RxJS operator helper

--- src/users/epics.ts
+++ src/users/epics.ts
@@ -1,13 +1,12 @@
-import { isActionOf } from 'typesafe-actions'
 import { combineEpics } from 'redux-observable'
 import { of } from 'rxjs'
-import { filter, map, switchMap, catchError } from 'rxjs/operators'
+import { map, switchMap, catchError } from 'rxjs/operators'
 
 import { Epic } from 'app/types'
 import Actions from 'app/actions'
 import Routes from 'app/routes'
 import { getJSON } from 'app/utils/ajax'
-import { onRouteEnter } from 'app/utils/epic'
+import { onRouteEnter, ofSafeType } from 'app/utils/epic'
 
 import { USERS_URL } from './constants'
 import { User } from './types'
@@ -19,7 +18,7 @@ const getDataOnRouteEnter: Epic = ($action) =>
 
 const getUsers: Epic = ($action) =>
   $action.pipe(
-    filter(isActionOf(Actions.Users.getUsers.request)),
+    ofSafeType(Actions.Users.getUsers.request),
     switchMap(() =>
       getJSON(USERS_URL).pipe(
         map((users) => Actions.Users.getUsers.success(users as User[])),
