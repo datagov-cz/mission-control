dbf4943fba0631ce21309ba644fe514fc06f4d88
Karel Klíma <karelklima@gmail.com>
2020-06-11 16:09:56 +0200

    [#34] Migrated users to a new ontology and API

--- src/app/vocabulary.ts
+++ src/app/vocabulary.ts
@@ -1,5 +1,5 @@
 const NS_POPIS_DAT =
-  'http://onto.fel.cvut.cz/ontologies/slovnik/agendovy/popis-dat/pojem/'
+  'http://onto.fel.cvut.cz/ontologies/slovník/agendový/popis-dat/pojem/'
 const NS_PRACOVNI_PROSTOR =
   'https://slovník.gov.cz/datový/pracovní-prostor/pojem/'
 
@@ -8,9 +8,9 @@ const ns = (prefix: string) => (suffix: string) => `${prefix}${suffix}`
 const nspd = ns(NS_POPIS_DAT)
 const nspp = ns(NS_PRACOVNI_PROSTOR)
 
-export const USER = nspd('uživatel')
-export const USER_ADMIN = nspd('administrátor')
-export const USER_DEACTIVATED = nspd('zablokovaný-uživatel')
+export const USER = nspp('uživatel')
+export const USER_ADMIN = nspp('administrátor')
+export const USER_DEACTIVATED = nspp('zablokovaný-uživatel')
 
 export const VOCABULARY_CONTEXT_READ_ONLY = nspp(
   'slovníkový-kontext-pouze-pro-čtení'
--- src/users/types.ts
+++ src/users/types.ts
@@ -2,7 +2,7 @@ export type UsersAction = import('users/actions').UsersAction
 
 export type UserData = {
   uri: string
-  types: string[]
+  types?: string[]
   username: string
   firstName: string
   lastName: string
