eb40f89fe695fd01023fafde138ae22dd9df4e3a
Karel Klíma <karelklima@gmail.com>
2020-09-06 18:20:25 +0200

    [#60] Replaced Netlify API proxy with direct API calls

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
 /**
  * API URL base
  */
-export const API_URL = process.env.REACT_APP_API_URL || '/sgov-server'
+export const API_URL = process.env.REACT_APP_API_URL || 'https://kbss.felk.cvut.cz/sgov-server'
