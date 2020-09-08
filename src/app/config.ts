104c89dff222ca04c61580f226294e377b3da9e4
Karel Klíma <karelklima@gmail.com>
2020-09-08 21:31:01 +0200

    [#60] Update code style in config.ts

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,5 @@
 /**
  * API URL base
  */
-export const API_URL = process.env.REACT_APP_API_URL || 'https://kbss.felk.cvut.cz/sgov-server'
+export const API_URL =
+  process.env.REACT_APP_API_URL || 'https://kbss.felk.cvut.cz/sgov-server'
