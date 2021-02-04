c2e5b1b8608b6c7fae099a604d6fc65e6f7c2672
Karel Klíma <karelklima@gmail.com>
2021-02-04 22:55:18 +0100

    [#107] Improved web page title

--- src/components/workspaces/Workspaces.tsx
+++ src/components/workspaces/Workspaces.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react'
 import { Typography, Container, Box } from '@material-ui/core'
 
 import t, { Namespace } from 'components/i18n'
+import Title from 'components/Title'
 
 import WorkspacesTable from './WorkspacesTable'
 import AddWorkspace from './AddWorkspace'
@@ -9,6 +10,7 @@ import AddWorkspace from './AddWorkspace'
 const Workspaces: React.FC = () => {
   return (
     <Namespace.Provider value="workspaces">
+      <Title id="workspaces.workspaces" />
       <Container className="Workspaces">
         <Box height="30px"></Box>
         <Box display="flex" flexDirection="row" justifyContent="space-between">
