a8fb667b323c063cfa933fc547dffd20aacc02ae
Karel Klíma <karelklima@gmail.com>
2021-02-04 23:14:50 +0100

    [#116] Set up standard OIDC authorization flow

--- src/app/variables.ts
+++ src/app/variables.ts
@@ -29,33 +29,22 @@ const APP_URL = CONTEXT === 'production' ? PRODUCTION_URL : DEVELOPMENT_URL
 const OIDC_AUTHORITY = getEnv('OIDC_AUTHORITY')
 const OIDC_CLIENT_ID = getEnv('OIDC_CLIENT_ID')
 
-const silentUri = (mainUri: string = APP_URL) =>
-  `${mainUri}/oidc-silent-callback.html`
-
-const signinUri = (mainUri: string, forwardUri: string) =>
-  `${mainUri}/oidc-signin-callback.html?forward_uri=${encodeURI(forwardUri)}`
-
-const dispatchUri = (forwardUri: string) =>
-  `${PRODUCTION_URL}/oidc-dispatch-redirect.html?dispatch_uri=${encodeURI(
-    signinUri(APP_URL, forwardUri)
-  )}`
-
 export const OIDC_CONFIG = {
   authority: OIDC_AUTHORITY,
   client_id: OIDC_CLIENT_ID,
-  redirect_uri: dispatchUri(APP_URL),
-  silent_redirect_uri: silentUri(),
+  redirect_uri: `${APP_URL}/oidc-signin-callback.html?forward_uri=${encodeURI(
+    APP_URL
+  )}`,
+  silent_redirect_uri: `${APP_URL}/oidc-silent-callback.html`,
   post_logout_redirect_uri: APP_URL,
-  response_type: 'token id_token',
+  response_type: 'code',
   loadUserInfo: true,
   automaticSilentRenew: true,
   revokeAccessTokenOnSignout: true,
 }
 
 export const generateRedirectUri = (forwardUri: string) =>
-  CONTEXT === 'production'
-    ? signinUri(PRODUCTION_URL, forwardUri)
-    : dispatchUri(forwardUri)
+  `${APP_URL}/oidc-signin-callback.html?forward_uri=${encodeURI(forwardUri)}`
 
 /**
  * Default vocabulary to include in each workspace
