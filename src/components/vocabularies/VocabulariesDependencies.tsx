f583cdfbb35bd2cdae849bc1f0981ff867a827e0
Karel Klíma <karelklima@gmail.com>
2021-04-21 14:57:33 +0200

    [#167] Improved vocabulary dependencies view

--- src/components/vocabularies/VocabulariesDependencies.tsx
+++ src/components/vocabularies/VocabulariesDependencies.tsx
@@ -6,9 +6,40 @@ import { flatten } from "lodash";
 import { getVocabularyShortLabel } from "@opendata-mvcr/assembly-line-shared";
 
 import { Iri } from "@types";
+import vocabularyTypes from "app/vocabularyTypes.json";
 
 import { useObservableSuspense } from "observable-hooks";
 import { workspaceVocabularyDependenciesResource } from "data/vocabularies";
+
+const getVocabularyLevel = (iri: Iri) => {
+  for (const type of vocabularyTypes) {
+    if (iri.startsWith(type.prefix)) {
+      return type.level;
+    }
+  }
+  return 0;
+};
+
+const getNormalizedLevelMapping = (vocabularies: Iri[]) => {
+  const levels = new Set<number>();
+  vocabularies.forEach((vocabulary) => {
+    levels.add(getVocabularyLevel(vocabulary));
+  });
+  const mapping = new Map<number, number>();
+  Array.from(levels)
+    .sort()
+    .forEach((key, index) => {
+      mapping.set(key, index);
+    });
+  return mapping;
+};
+
+const getNormalizedVocabularyLevel = (
+  iri: Iri,
+  normalizedMapping: Map<number, number>
+) => {
+  return normalizedMapping.get(getVocabularyLevel(iri));
+};
 
 const buildGraph = (dependencies: Record<Iri, Iri[]>): Data => {
   const nodes: Node[] = [];
@@ -19,11 +50,18 @@ const buildGraph = (dependencies: Record<Iri, Iri[]>): Data => {
     mainVocabularies.map((key) => dependencies[key])
   ).filter((dep) => !mainVocabularies.includes(dep));
 
+  const normalizedLevelMapping = getNormalizedLevelMapping(
+    mainVocabularies.concat(dependencyVocabularies)
+  );
+
+  console.log(normalizedLevelMapping);
+
   mainVocabularies.forEach((iri) => {
     nodes.push({
       id: iri,
       label: getVocabularyShortLabel(iri) || iri,
       color: "#FFFFFF",
+      level: getNormalizedVocabularyLevel(iri, normalizedLevelMapping),
     });
   });
 
@@ -32,6 +70,7 @@ const buildGraph = (dependencies: Record<Iri, Iri[]>): Data => {
       id: iri,
       label: getVocabularyShortLabel(iri) || iri,
       color: "#FFD500",
+      level: getNormalizedVocabularyLevel(iri, normalizedLevelMapping),
     });
   });
 
@@ -62,7 +101,11 @@ const graphOptions = {
     shape: "box",
   },
   edges: {
-    smooth: false,
+    smooth: {
+      enabled: true,
+      type: "curvedCW",
+      roundness: 0.15,
+    },
     color: "#000000",
     width: 0.5,
     arrows: {
@@ -71,12 +114,18 @@ const graphOptions = {
         scaleFactor: 0.5,
       },
     },
+    type: "curvedCW",
   },
   layout: {
+    improvedLayout: true,
     hierarchical: {
-      enabled: false,
+      enabled: true,
       nodeSpacing: 200,
+      levelSeparation: 100,
     },
+  },
+  physics: {
+    enabled: false,
   },
 };
 
